81856476dbe7a8af7c9d78837f4d73b3bb4bef8c
yasin-ait <yasin@ait.com.tr>
2024-08-09 12:16:34 +0300

    moved

--- utils/utils.py
+++
@@ -1,9 +0,0 @@
-import torchvision.transforms.functional as TF
-
-class InvertMaskColors(object):
-    def __call__(self, mask):
-        if mask.mode != 'L':
-            raise ValueError("Input mask should be in grayscale (L) mode")
-        mask = TF.to_tensor(mask)
-        mask = 1.0 - mask
-        return TF.to_pil_image(mask)
